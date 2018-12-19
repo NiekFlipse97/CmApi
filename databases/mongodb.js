a72a6576e9bb612e7b2a793ee547e1da4942d236
dmbuzink <dmbuzink@avans.nl>
2018-12-19 12:18:16 +0100

    Connection to online mongo database added

---
+++ databases/mongodb.js
@@ -0,0 +1,13 @@
+const config = require('../config/config');
+const mongoose = require('mongoose');
+
+// changes to use some newer underlying functionality to prevent deprecation problems
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.databases.mongo_atlas, {useNewUrlParser: true})
+    .then(() => {
+        console.log("Connection to the production Mongo database at Atlas has been made");
+    })
+    .catch((err) => console.log(err));
