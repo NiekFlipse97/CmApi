b0976b6dabe4b8bf1b53a3259edc84bb60b56466
Unknown <ywillems96@gmail.com>
2019-01-07 12:54:30 +0100

    Commented out bcrypt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,21 +7,24 @@ function login(req, res) {
     let username = req.body.username || ''
     let password = req.body.password || ''
 
-    User.findOne({ username: username })
+    User.findOne({ username: username, password: password })
         .then(user => {
             console.log(password + " ....... " + user.password)
-            bcrypt.compare(password, user.password)
-                .then(match => {
-                    if(!match) return res.status(401).json(Errors.unauthorized())
+            //     bcrypt.compare(password, user.password)
+            //         .then(match => {
+            //             if(!match) return res.status(401).json(Errors.unauthorized())
 
-                    token = auth.encodeToken(user._id)
-                    res.status(200).json({token: token})
+            //             token = auth.encodeToken(user._id)
+            //             res.status(200).json({token: token})
 
-                })
-                .catch(err => {
-                    res.status(401).json(err)
-                })
+            //         })
+            //         .catch(err => {
+            //             res.status(401).json(err)
+            //         })
 
+            // })
+            token = auth.encodeToken(user._id)
+            res.status(200).json({ token: token })
         })
         .catch(err => {
             res.status(401).json(Errors.unauthorized())
